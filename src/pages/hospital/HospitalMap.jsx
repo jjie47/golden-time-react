b1f54cf95b651bbb84305a458c9718d5db83fbe3
jjie47 <niuiu47@gmail.com>
2024-11-25 10:24:10 +0900

    feat: 병원 조회 API 연동 및 지역별,병원명 검색

---
+++ src/pages/hospital/HospitalMap.jsx
@@ -0,0 +1,23 @@
+const HospitalMap = () => {
+    // const x = document.getElementById("demo");
+
+    // function getLocation() {
+    // if (navigator.geolocation) {
+    //     navigator.geolocation.getCurrentPosition(showPosition);
+    // } else {
+    //     console.log("Geolocation is not supported by this browser.");
+    // }
+    // }
+
+    // console.log("Latitude: ", position.coords.latitude)
+    // console.log("Longitude: ", position.coords.longitude)
+
+    return (
+        <>
+            <div className="map">
+                        
+            </div>
+        </>
+    );
+}
+export default HospitalMap;
